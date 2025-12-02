3ec4373f937dcbecb73255b5e90bef02ba1631ef
leduc123 <leanhduccpr2000@gmail.com>
2025-12-02 08:26:39 +0700

    Add feature: add_song

--- playlist.py
+++ playlist.py
@@ -1,9 +1,16 @@
-a = int(input("Nhập cạnh a: "))
-b = int(input("Nhập cạnh b: "))
-c = int(input("Nhập cạnh c: "))
 
-if a + b > c and a + c > b and b + c > a:
-    print("3 cạnh tạo thành tam giác")
-else:
-    print("Không phải tam giác")
+def add_song():
+    title = input("Nhập tên bài hát: ")
+    artist = input("Nhập tên ca sĩ: ")
+    duration = int(input("Nhập thời lượng (giây): "))
+
+    songs.append({
+        'title': title,
+        'artist': artist,
+        'duration': duration
+    })
+
+    print("Đã thêm bài hát vào playlist.")
+
+
     
