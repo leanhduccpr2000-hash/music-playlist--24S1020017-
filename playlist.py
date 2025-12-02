75af6d402e2cf25a6a35d174769f97d847690f78
leduc123 <leanhduccpr2000@gmail.com>
2025-12-02 08:29:29 +0700

    Add feature: view_playlist

--- playlist.py
+++ playlist.py
@@ -1,16 +1,13 @@
 
-def add_song():
-    title = input("Nhập tên bài hát: ")
-    artist = input("Nhập tên ca sĩ: ")
-    duration = int(input("Nhập thời lượng (giây): "))
-
-    songs.append({
-        'title': title,
-        'artist': artist,
-        'duration': duration
-    })
-
-    print("Đã thêm bài hát vào playlist.")
+ 
+def view_playlist():
+    if not songs:
+        print("Playlist đang trống.")
+        return
+    
+    print("\n--- DANH SÁCH PHÁT ---")
+    for i, song in enumerate(songs, start=1):
+        print(f"{i}. {song['title']} - {song['artist']} ({song['duration']}s)")
 
 
     
